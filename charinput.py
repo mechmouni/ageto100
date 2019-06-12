75f06328235f0e9550d3d246e17115cd590c5f6c
Mouni Reddy <mechmouni@live.com>
2019-06-12 06:40:52 -0700

    Bug fix: changed year type to int

--- charinput.py
+++ charinput.py
@@ -1,11 +1,12 @@
 from datetime import date
 
 name = input("What is your name?")
-age = input("What is your age")
+age = int(input("What is your age"))
 
 def calcto100(name, age):
-    100-age
-    year100 = date.today().strftime("%Y") + (100 - age)
+    
+    year100 = int(date.today().strftime("%Y")) + (100 - age)
 
     return print ("{} will turn 100 in {}".format(name, year100))
     
+calcto100(name, age)
